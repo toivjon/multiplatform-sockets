0805ac1a418efd14be1a664e8160bf52ca239cd9
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-08-15 09:13:23 +0300

    Moved some parts of the UDP example values as constants.

--- examples/udp_client/main.cpp
+++ examples/udp_client/main.cpp
@@ -4,15 +4,19 @@
 
 using namespace mps;
 
+constexpr auto BufferSize = 1024;
+constexpr auto Port = 5555;
+constexpr auto IPAddress = "127.0.0.1";
+
 int main() {
-    char buffer[1024];
+    char buffer[BufferSize];
 
     UDPSocket socket(AddressFamily::IPv4);
     
     std::cout << "Sending 'hello' message to server..." << std::endl;
-    socket.sendTo(Address("127.0.0.1", 5555), reinterpret_cast<const void*>("hello"), 5);
+    socket.sendTo(Address(IPAddress, Port), reinterpret_cast<const void*>("hello"), 5);
     
-    socket.recvFrom(buffer, 1024);
+    socket.recvFrom(buffer, BufferSize);
     buffer[5] = '\0';
     std::cout << "Message from the server: " << buffer << std::endl; 
     return 0;
--- examples/udp_server/main.cpp
+++ examples/udp_server/main.cpp
@@ -4,20 +4,24 @@
 
 using namespace mps;
 
+constexpr auto BufferSize = 1024;
+constexpr auto Port = 5555;
+constexpr auto IPAddress = "127.0.0.1";
+
 int main() {
-    char buffer[1024];
+    char buffer[BufferSize];
 
     UDPSocket socket(AddressFamily::IPv4);
-    socket.bind(Address("127.0.0.1", 5555));
+    socket.bind(Address(IPAddress, Port));
 
-    std::cout << "Listening for incoming messages on port " << 5555 << std::endl;
-    auto senderAddress = socket.recvFrom(buffer, 1024);
+    std::cout << "Listening for incoming messages on port " << Port << std::endl;
+    auto senderAddress = socket.recvFrom(buffer, BufferSize);
     buffer[5] = '\0';
     std::cout << "Message from the client: " << buffer << std::endl;
 
     std::cout << "Sending a 'thx!' message to client" << std::endl;
     socket.sendTo(senderAddress, reinterpret_cast<const void*>("thx!"), 4);
-    socket.recvFrom(buffer, 1024);
+    socket.recvFrom(buffer, BufferSize);
     
     return 0;
 }
