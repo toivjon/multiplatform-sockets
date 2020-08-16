aa3c56c87bf199308014cc1558a3a7799f70e44b
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-08-16 14:02:06 +0300

    Added a support for the client example to take server host and port as args.

--- examples/udp_client/main.cpp
+++ examples/udp_client/main.cpp
@@ -6,17 +6,25 @@
 using namespace mps;
 
 constexpr auto BufferSize = 1024;
-constexpr auto Port = 5555;
-constexpr auto IPAddress = "127.0.0.1";
 
-int main() {
+int main(int argc, char* argv[]) {
+  // sanity check to check that we have enough arguments.
+  if (argc != 3) {
+    std::cout << "Usage: <executable> <host> <port>" << std::endl;
+    return -1;
+  }
+
+  // parse host and port from the commandline arguments.
+  auto host = std::string(argv[1]);
+  auto port = std::stoi(argv[2]);
+  auto addr = Address(host, port);
+
   char buffer[BufferSize];
   std::string message = "hello";
-  auto serverAddress = Address(IPAddress, Port);
   try {
     UDPSocket socket(AddressFamily::IPv4);
     std::cout << "Sending '" << message << "' to server..." << std::endl;
-    socket.sendTo(serverAddress, message);
+    socket.sendTo(addr, message);
     std::cout << "Waiting for server to respond..." << std::endl;
     socket.recvFrom(buffer, BufferSize);
     buffer[4] = '\0';
