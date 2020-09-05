33cad84b1c7c9795c3bf39862fef8a62a588ebef
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-05 10:40:46 +0300

    Improved the information output within the UDP socket server example.

--- examples/udp_server/main.cpp
+++ examples/udp_server/main.cpp
@@ -10,9 +10,16 @@ constexpr auto SocketPort = 56789;
 
 int main() {
   try {
-    // build a new UDP socket and print out the bound address.
+    // build and bind a new UDP socket and print out the socket info.
     UDPSocket socket(SocketPort, SocketAddressFamily);
-    std::cout << "Server: " << socket.getLocalAddress() << std::endl;
+    std::cout << "Bound a new UDP socket with following details:" << std::endl;
+    std::cout << "    local-ip: " << socket.getLocalIP() << std::endl;
+    std::cout << "  local-port: " << socket.getLocalPort() << std::endl;
+    std::cout << "    blocking: " << socket.isBlocking() << std::endl;
+    std::cout << "     routing: " << socket.isRouting() << std::endl;
+    std::cout << "broadcasting: " << socket.isBroadcasting() << std::endl;
+    std::cout << " recvBufSize: " << socket.getReceiveBufferSize() << std::endl;
+    std::cout << " sendBufSize: " << socket.getSendBufferSize() << std::endl;
 
     auto packet = socket.receive(BufferSize);
     std::vector<uint8_t> buffer(packet.data);
