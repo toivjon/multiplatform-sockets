65bfe601036fc17ae1dcf972a6cf9b5ff564a57a
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-05 18:25:25 +0300

    Added some more info printouts to TCP server example.

--- examples/tcp_server/main.cpp
+++ examples/tcp_server/main.cpp
@@ -4,19 +4,35 @@
 
 using namespace mps;
 
-constexpr auto BufferSize = 1024;
+constexpr auto SocketAddressFamily = AddressFamily::IPv4;
 constexpr auto SocketPort = 56789;
 
 int main() {
   try {
-    // build a new TCP socket and print out the bound address.
-    TCPServerSocket socket(SocketPort);
-    std::cout << "Server: " << socket.getLocalAddress() << std::endl;
+    // build and bind a new TCP socket and print out the socket info.
+    TCPServerSocket socket(SocketPort, SocketAddressFamily);
+    std::cout << "Bound a new UDP socket with following details:" << std::endl;
+    std::cout << "    local-ip: " << socket.getLocalIP() << std::endl;
+    std::cout << "  local-port: " << socket.getLocalPort() << std::endl;
+    std::cout << "    blocking: " << socket.isBlocking() << std::endl;
+    std::cout << "     routing: " << socket.isRouting() << std::endl;
+    std::cout << "    bufferin: " << socket.isBuffering() << std::endl;
+    std::cout << " recvBufSize: " << socket.getReceiveBufferSize() << std::endl;
+    std::cout << " sendBufSize: " << socket.getSendBufferSize() << std::endl;
 
+    // wait for an incoming TCP client connection and then print client info.
+    std::cout << "Waiting for incoming TCP client connection..." << std::endl;
     auto client = socket.accept();
-    std::cout << "Client local  IP=" << client.getLocalIP() << " port=" << client.getLocalPort() << std::endl;
-    std::cout << "Client remote IP=" << client.getRemoteIP() << " port=" << client.getRemotePort() << std::endl;
-    client.setKeepAlive(true);
+    std::cout << "Received a TCP client with following details:" << std::endl;
+    std::cout << "    local-ip: " << client.getLocalIP() << std::endl;
+    std::cout << "  local-port: " << client.getLocalPort() << std::endl;
+    std::cout << "   remote-ip: " << client.getRemoteIP() << std::endl;
+    std::cout << " remote-port: " << client.getRemotePort() << std::endl;
+    std::cout << "    blocking: " << client.isBlocking() << std::endl;
+    std::cout << "     routing: " << client.isRouting() << std::endl;
+    std::cout << "    bufferin: " << client.isBuffering() << std::endl;
+    std::cout << " recvBufSize: " << client.getReceiveBufferSize() << std::endl;
+    std::cout << " sendBufSize: " << client.getSendBufferSize() << std::endl;
 
     auto data = client.receive();
     std::vector<uint8_t> buffer(data);
