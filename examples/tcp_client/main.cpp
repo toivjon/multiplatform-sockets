eccfc45fb896bd7964cb48c1760eaaf182f7a110
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-05 18:33:08 +0300

    Improved the exception handling in the TCP client example.

--- examples/tcp_client/main.cpp
+++ examples/tcp_client/main.cpp
@@ -13,12 +13,12 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  // parse host and port from the commandline arguments.
-  auto host = std::string(argv[1]);
-  auto port = std::stoi(argv[2]);
-  auto addr = Address(host, port);
-
   try {
+    // parse host and port from the commandline arguments.
+    auto host = std::string(argv[1]);
+    auto port = std::stoi(argv[2]);
+    auto addr = Address(host, port);
+
     // build and connect the client into destination and print bound addresses.
     TCPClientSocket socket(addr);
     std::cout << " Local Address: " << socket.getLocalAddress() << std::endl;
@@ -27,8 +27,10 @@ int main(int argc, char* argv[]) {
     socket.send({ 'h','e','l','l','o', '\0' });
     auto data = socket.receive(BufferSize);
     std::cout << "response: " << &data[0] << std::endl;
+  } catch (const AddressException& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
   } catch (const SocketException& e) {
-    std::cerr << e.what() << std::endl;
+    std::cerr << "Error: " << e.what() << std::endl;
   }
   return 0;
 }
