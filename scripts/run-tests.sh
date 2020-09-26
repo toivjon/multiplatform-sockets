3eefd4446988941cf06d1a8c5f3b9576161a1eae
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 18:38:27 +0300

    Added -Wall and -x c++ compilation flags for clang-tidy.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 if [ $TRAVIS_OS_NAME = "linux" ]; then
   cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 .
-  clang-tidy -checks=* ./include/mps.h -- -std=c++11
+  clang-tidy -checks=* ./include/mps.h -- -Wall -std=c++11 -x c++
 fi
 cmake -S . -B build
 cmake --build build
