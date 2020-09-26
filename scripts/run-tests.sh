f6a804cbd7d16f43ada0e38a49895e0443f5659e
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 10:27:10 +0300

    Moved cppcheck check into the run-tests script.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,3 +4,8 @@ cmake -S . -B build
 cmake --build build
 cd build
 ctest
+
+if [ $TRAVIS_OS_NAME = "linux" ]
+  cd ..
+  cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 . 
+fi
