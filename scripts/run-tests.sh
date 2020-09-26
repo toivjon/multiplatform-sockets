67de0151225602a39c22451409c55605ae76cc73
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 18:15:56 +0300

    Added the -std=c++11 flag for the clang-tidy.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 if [ $TRAVIS_OS_NAME = "linux" ]; then
   cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 .
-  clang-tidy -checks=* ./include/mps.h --
+  clang-tidy -checks=* ./include/mps.h -- -std=c++11
 fi
 cmake -S . -B build
 cmake --build build
