078735f5e29b22692a818b0640ef3104e169b66b
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 17:31:40 +0300

    Perform static code analyze before building and running the tests.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -ev
+if [ $TRAVIS_OS_NAME = "linux" ]; then
+  cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 .
+fi
 cmake -S . -B build
 cmake --build build
 cd build
 ctest
-
-if [ $TRAVIS_OS_NAME = "linux" ]; then
-  cd ..
-  cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 .
-fi
