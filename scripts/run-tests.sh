ded6b2a44062428d94afc1e4fddc1da2b6652b6a
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 18:02:18 +0300

    Added -- at the end of the clang-tidy call.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 if [ $TRAVIS_OS_NAME = "linux" ]; then
   cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 .
-  clang-tidy ./include/mps.h -checks=*
+  clang-tidy -checks=* ./include/mps.h --
 fi
 cmake -S . -B build
 cmake --build build
