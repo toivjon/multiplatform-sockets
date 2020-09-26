b37284d5aa21a6d15f456d03a9a1b87a764eefd4
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 10:31:22 +0300

    Fix for the script if-else-structure.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ cmake --build build
 cd build
 ctest
 
-if [ $TRAVIS_OS_NAME = "linux" ]
+if [ $TRAVIS_OS_NAME = "linux" ]; then
   cd ..
   cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 . 
 fi
