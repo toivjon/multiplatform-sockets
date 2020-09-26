31c5dd071ba5a164293045c2f68629fdaf1ee728
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-09-26 17:51:47 +0300

    Added clang-format check into linux build.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,6 +2,7 @@
 set -ev
 if [ $TRAVIS_OS_NAME = "linux" ]; then
   cppcheck -q -I include -i build -i examples --enable=all --suppress=missingIncludeSystem --error-exitcode=1 .
+  clang-format -style=LLVM *.cpp *.h
 fi
 cmake -S . -B build
 cmake --build build
